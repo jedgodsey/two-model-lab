58717a9bc3c3f8bca28bc29d5b7b6ba750806824
jedgodsey <jed.godsey@gmail.com>
2020-10-06 16:06:17 -0700

    added view engine, controllers and middleware

--- server.js
+++ server.js
@@ -8,4 +8,20 @@ const morgan = require('morgan');
 require('dotenv').config();
 const PORT = process.env.PORT || 6000;
 
+//view engine
+app.set('view engine', 'ejs');
+
+// set controllers
+const ctrl = require('./controllers');
+
+//middleware
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json()); //what does this do?
+app.use(morgan('tiny')); //what does morgan do?
+app.use(methodOverride('_method')); // syntax in the string?
+
+//home route
+
+//404 route
+
 app.listen(PORT, console.log(`listening on port ${PORT}`))
