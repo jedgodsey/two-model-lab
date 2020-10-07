17e1b64b565fba81c9f1bf1629883230374948a5
jedgodsey <jed.godsey@gmail.com>
2020-10-06 20:46:47 -0700

    finished 7 routes for schools

--- controllers/schoolCtrl.js
+++ controllers/schoolCtrl.js
@@ -4,17 +4,56 @@ const db = require('../models');
 
 // current path assumes '/schools'?
 
-router.get('/', (res, req) => {
-    
+router.get('/', (req, res) => {
+    db.School.find({}, (error, schools) => {
+        error ? console.log(error) : res.render('schools/index', {schools})
+    })
 })
 
+router.get('/new', (req, res) => {
+    res.render('schools/new')
+})
 
+router.post('/', (req, res) => {
+    db.School.create(req.body, (error, newSchool) => {
+        error ? console.log(error) : res.redirect(`/schools/${newSchool.id}`)
+    })
+})
 
+router.get('/:school', (req, res) => {
+    db.School.findById(req.params.school, (error, school) => {
+        const context = {
+            school: school.school,
+            location: school.location,
+            public: school.public
+        }
+        error ? console.log(error) : res.render('schools/show', context)
+    })
+})
 
+router.delete('/:school', (req, res) => {
+    db.School.findByIdAndDelete(req.params.school, (error, school) => {
+        error ? console.log(error) : res.redirect('/schools');
+    })
+})
 
+router.get('/:school/edit', (req, res) => {
+    db.School.findById(req.params.school, (error, school) => {
+        const context = {
+            school: school.school,
+            location: school.location,
+            public: school.public
+        }
+        error ? console.log(error) : res.render('schools/show', context)
+    })
+})
 
-
-
-
-
+router.put('/:school', (req, res) => {
+    db.School.findByIdAndUpdate(req.params.school,
+        req.body,
+        {new: true},
+        (error, school) => {
+        error ? console.log(error) : res.redirect(`/articles/${school._id}`)
+    })
+})
 module.exports = router;
